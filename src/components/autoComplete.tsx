3a094a935c3949b02b96325ed1f77ba618e265e6
Kellswork <kellogbonna@gmail.com>
2024-01-14 11:34:02 +0100

    convert input to react controlled input

--- src/components/autoComplete.tsx
+++ src/components/autoComplete.tsx
@@ -1,10 +1,23 @@
-const AutoComplete = () => {
-  return (
-    <div className="autocomplete-container">
-      <input type="text" placeholder="Search users" />
-      <ul className="suggestions"></ul>
-    </div>
-  );
-};
+import { ChangeEvent, useState } from 'react'
 
-export default AutoComplete;
+function AutoComplete() {
+  const [searchText, setSearchText] = useState<string>("");
+
+  const handleInputChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.currentTarget.value);
+  };
+
+    return (
+        <div className="autocomplete-container">
+            <input
+                type="text"
+                value={searchText}
+                onChange={handleInputChange}
+                placeholder="Search users"
+            />
+            <ul className="suggestions"></ul>
+        </div>
+    )
+}
+
+export default AutoComplete
