9cef772a6b7b7a1c7dc3210dc9c0749d543aa9ca
Kellswork <kellogbonna@gmail.com>
2024-03-11 22:05:12 +0100

    fix apiendpoint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import ResponseData from "./type";
 
 export const fetchData = async () => {
   try {
-    const response = await fetch("https://jsonplaceholder.typicod.com/users");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const responseData = (await response.json()) as ResponseData[];
     return responseData;
   } catch (error) {
